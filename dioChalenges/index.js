2ac663723dfe32f3c1c042fe37dd50f52eeaad42
Gabriel Moura <gabrielnfl13@gmail.com>
2022-04-26 15:33:22 -0300

    adding new exercises of try catch error

--- dioChalenges/index.js
+++ dioChalenges/index.js
@@ -1,15 +1,15 @@
 const myMap = new Map();
 
-myMap.set('apple', 'fruit');
+myMap.set("apple", "fruit");
 // Map(1) {"apple" => "fruit"}
 
-myMap.get('apple');
+myMap.get("apple");
 // "fruit"
 
-myMap.delete('apple');
+myMap.delete("apple");
 // true
 
-myMap.get('apple');
+myMap.get("apple");
 // undefined
 
 const mySet = new Set();
@@ -30,7 +30,7 @@ function getAdmins(map) {
   let admins = [];
 
   for ([key, value] of map) {
-    if (value === 'Admin') {
+    if (value === "Admin") {
       admins.push(key);
     }
   }
@@ -42,7 +42,7 @@ function getUsers(map) {
   let users = [];
 
   for ([key, value] of map) {
-    if (value === 'User') {
+    if (value === "User") {
       users.push(key);
     }
   }
@@ -52,17 +52,17 @@ function getUsers(map) {
 
 const usuarios = new Map();
 
-usuarios.set('Gabriel', 'Admin');
+usuarios.set("Gabriel", "Admin");
 
-usuarios.set('Ana', 'User');
+usuarios.set("Ana", "User");
 
-usuarios.set('Victoria', 'Admin');
+usuarios.set("Victoria", "Admin");
 
-usuarios.set('Luiz', 'User');
+usuarios.set("Luiz", "User");
 
-usuarios.set('José', 'User');
+usuarios.set("José", "User");
 
-usuarios.set('Pedro', 'user');
+usuarios.set("Pedro", "user");
 
 console.log(getAdmins(usuarios));
 
@@ -82,19 +82,19 @@ console.log(valoresUnicos(meuArray));
 
 // using return
 function palindromeVerify(string) {
-  if (!string) return 'Invalid string';
+  if (!string) return "Invalid string";
 
-  return string === string.split('').reverse().join('');
+  return string === string.split("").reverse().join("");
 }
 
-palindromeVerify('cat');
+palindromeVerify("cat");
 
 console.log(palindromeVerify);
 
 function palindromeIsOk(string) {
-  if (!string) throw 'Invalid string';
+  if (!string) throw "Invalid string";
 
-  return string === string.split('').reverse().join('');
+  return string === string.split("").reverse().join("");
 }
 
 // palindromeIsOk('');
@@ -104,8 +104,58 @@ function tryCatchExample(string) {
     palindromeIsOk(string);
   } catch (e) {
     throw e;
-  }
-  finally {
-    console.log('A string enviada foi: ' + string);
+  } finally {
+    console.log("A string enviada foi: " + string);
   }
 }
+
+// construindo o Objeto error => new Error(message, fileName, lineNumber)
+
+const MyError = new Error("Mensagem Inválida");
+MyError.name = "Invalid<essage";
+
+// throw MyError;
+
+// practice error
+
+function validateArray(arr, num) {
+  try {
+    if (!arr && !num) 
+      throw new ReferenceError("Send paramenters");
+
+    if (typeof arr !== "object")
+      throw new TypeError("Array needs to be object type");
+
+    if (typeof num !== "number")
+      throw new TypeError("Num needs to be number type");
+
+    if (arr.length !== num)
+      throw new RangeError("invalid lenght");
+
+    return arr;
+
+  } catch (e) {
+    if (e instanceof ReferenceError) {
+      console.log("This is a ReferenceError");
+
+      console.log(e.message);
+
+    } else if (e instanceof TypeError) {
+      console.log("This is a TypeError");
+      
+      console.log(e.message);
+    } else if (e instanceof RangeError) {
+      console.log("This is a RangeError");
+      
+      console.log(e.message);
+    } else {
+      console.log("Error type unkown: " + e);
+    }
+  }
+}
+
+console.log(validateArray());
+
+console.log(validateArray([], 5));
+
+console.log(validateArray([1,2,3,4,5,6,7], 5));
