6d239cbc97b4b80fc4ce34955c3a07b1eebd5f25
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-05-08 11:08:24 -0300

    adding simple library system to portfolio

--- discoverRocketseat/script.js
+++ discoverRocketseat/script.js
@@ -488,3 +488,112 @@ try {
 console.log()
 console.log('<----Exercise4---->'); // 
 console.log()
+
+/*
+   ## Buscandoecontando dados em Arrays 0##
+
+  Baseado no Array de Livros por Categoria abaixo, faça os seguintes
+  desafios:
+
+    •Contar o número de categorias e o número de livros em cada 
+    categoria
+    •Contar o número de autores
+    •Mostrar livros do autor Auguto Cury
+    •Transformar a função acima em uma função que irá receber o nome 
+    do autor e devolver os livros desse autor.
+*/
+
+const booksByCategory=[
+  {
+    nameCategory: "Riqueza",
+    books: [
+      {
+        title: "Os segredos da mente milionária",
+        author: "T. Harv Eker",
+      },
+      {
+        title: "0 homem mais rico da Babilônia",
+        author: "George S. Clason",
+      },
+      {
+        title: "Pai rico, pai pobre",
+        author: "Robert T. KiyosakieSharon L. Lechter",
+      },
+    ],
+  },
+  {
+    nameCategory: "Inteligência Emocional",
+    books: [
+      {
+        title: "Você é Insubstituível",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Ansiedade - Como enfrentar o mal do século",
+        author: "Augusto Cury",
+      },
+      {
+        title: "Os 7 hábitos das pessoas altamente eficazes",
+        author: "Stephen R. Covey",
+      },
+    ]
+  },
+];
+
+const totalCategories = booksByCategory.length
+
+console.log('Total number of category:')
+console.log(totalCategories)
+
+for (let category of booksByCategory) {
+  console.log('Total books of category:', category.nameCategory)
+  console.log(category.books.length)
+}
+
+function countAuthors() {
+  let authors = []
+
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (authors.indexOf(book.author) == -1) {
+        authors.push(book.author)
+      }
+    }
+  }
+
+  console.log("Total authors: ", authors.length)
+}
+
+countAuthors()
+
+function booksOfAusgustoCury() {
+  let books = [];
+
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (book.author === 'Augusto Cury') {
+        books.push(book.title)
+      }
+    }
+  }
+
+  console.log("Author's books: ", books)
+}
+
+booksOfAusgustoCury()
+
+function booksOfAuthor(author) {
+  let books = [];
+
+  for (let category of booksByCategory) {
+    for (let book of category.books) {
+      if (book.author === author) {
+        books.push(book.title)
+      }
+    }
+  }
+
+  console.log(`${author}'s books: ${books.join(", ")}`)
+}
+
+booksOfAuthor('Stephen R. Covey')
