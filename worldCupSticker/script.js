889d8b87d1e511e2d7a4113e798212c848616965
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-10-28 16:02:36 -0300

    igniteLab worldCupCard

---
+++ worldCupSticker/script.js
@@ -0,0 +1,10 @@
+let isIgnite = true;
+
+function changeCard(event) {
+  const card = event.currentTarget;
+
+  const bg = isIgnite ? 'explorer' : 'ignite';
+  isIgnite = !isIgnite;
+
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg`;
+}
