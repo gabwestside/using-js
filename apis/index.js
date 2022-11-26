6c13ca307981154c32ec2231992c83198f1da75e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-11-26 19:53:00 -0300

    feat: adding get require

--- apis/index.js
+++ apis/index.js
@@ -3,3 +3,6 @@ const express = require('express')
 const app = express()
 
 app.listen('3000')
+
+app.route('/').get( ( req, res ) => res.send('home') )
+app.route('/about').get( ( req, res ) => res.send('about server') )
