22d61c1ea96e9a78af38939faf0d39eb889fe450
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-11-30 22:17:22 -0300

    feat: learning how to use post require

--- apis/index.js
+++ apis/index.js
@@ -6,3 +6,8 @@ app.listen('3000')
 
 app.route('/').get( ( req, res ) => res.send('home') )
 app.route('/about').get( ( req, res ) => res.send('about server') )
+
+//middleware
+app.use(express.json())
+
+app.route('/').post( ( req, res ) =>  res.send(req.body))
